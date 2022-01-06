5f7795f2398e182416afe3bc35601817e91385d4
sugmoto <a.sugmoto@gmail.com>
2022-01-06 22:15:37 +0900

    無限ロード修正

--- wp-content/themes/siesta/assets/js/app.js
+++ wp-content/themes/siesta/assets/js/app.js
@@ -10,41 +10,50 @@ $(function(){
 
 /*load more
 ===============================================================*/
-let now_post_num = 15; // 現在表示されている件数
-let get_post_num = 18; // もっと読み込むボタンで取得する件数
+if($('.articleList').length) {
+    var loadFlg = false;
+    let now_post_num = 15; // 現在表示されている件数
+    let get_post_num = 18; // もっと読み込むボタンで取得する件数
+    var offset = 0.5;
+    var timeoutId;
 
-//archive側で設定したdata属性の値を取得
-let init = $(".init");
-let load = $(".load");
-let all_count = load.data("count"); //カスタム投稿の全件数
+    //archive側で設定したdata属性の値を取得
+    let init = $(".init");
+    let load = $(".load");
+    let all_count = load.data("count"); //カスタム投稿の全件数
 
-//admin_ajaxにadmin-ajax.phpの絶対パス指定（相対パスは失敗する）
-let host_url = location.protocol + "//" + location.host;
-let admin_ajax = host_url + '/blog.siesta/wp-admin/admin-ajax.php';
+    //admin_ajaxにadmin-ajax.phpの絶対パス指定（相対パスは失敗する）
+    let host_url = location.protocol + "//" + location.host;
+    let admin_ajax = host_url + '/wp-admin/admin-ajax.php';
 
-$(window).on('scroll', function () {
-    var docH = $(document).innerHeight(); //ページ全体の高さ
-    var winH = $(window).innerHeight(); //ウィンドウの高さ
-    var bottom = docH - winH; //ページ全体の高さ - ウィンドウの高さ = ページの最下部位置
-    if (bottom <= $(window).scrollTop()) {
-        
-        //一番下までスクロールした時に実行
-        $.ajax({
-            type: 'POST',
-            url: admin_ajax,
-            data: {
-                'action' : 'my_ajax_action', //functions.phpで設定する関数名
-                'now_post_num': now_post_num,
-                'get_post_num': get_post_num
-            },
-        })
-        .done(function(data){
-            init.append(data);
-            now_post_num = now_post_num + get_post_num; 
-        })
-        .fail(function(){
-            alert('error');
-        })
+    $(window).on('scroll', function () {
+        clearTimeout(timeoutId);
 
-    }
-});
+        timeoutId = setTimeout( function () {
+            var docH = $(document).innerHeight(); //ページ全体の高さ
+            var winH = $(window).innerHeight(); //ウィンドウの高さ
+            var bottom = docH - winH; //ページ全体の高さ - ウィンドウの高さ = ページの最下部位置
+
+            if (bottom <= ($(window).scrollTop() + offset)) {
+                //一番下までスクロールした時に実行
+                $.ajax({
+                    type: 'POST',
+                    url: admin_ajax,
+                    data: {
+                        'action' : 'my_ajax_action', //functions.phpで設定する関数名
+                        'now_post_num': now_post_num,
+                        'get_post_num': get_post_num
+                    },
+                })
+                .done(function(data){
+                    init.append(data);
+                    now_post_num = now_post_num + get_post_num; 
+                })
+                .fail(function(){
+                    alert('error');
+                })
+            }
+        },250);
+
+    });
+}
--- wp-content/themes/siesta/footer.php
+++ wp-content/themes/siesta/footer.php
@@ -1,6 +1,6 @@
 <script src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/js/jquery-3.6.0.min.js"></script>
 <script src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/js/swiper-bundle.min.js"></script>
-<script src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/js/app.js"></script>
+<script src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/js/app.js?20220106"></script>
 
 <?php wp_footer(); ?>
 
